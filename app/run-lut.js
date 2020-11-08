1d5f37b4852a8ac7f792b768bf5c516c705352e2
Felix Akkermans <me+code@felixakkermans.com>
2020-11-08 23:39:48 +0100

    Add travel time & distance to run

---
+++ app/run-lut.js
@@ -0,0 +1,63 @@
+// organization id -> { address, travel time } lookup
+export const RUN_ADDRESS_LUT = {
+  1: {
+    address: 'Niersdal Survivalrun, Niersdijk, 6591 DL Gennep',
+    travelTime: 111
+  },
+  2: {
+    address: 'Streker Survivalrun Vereniging, Wijzend 52, 1613 KP Grootebroek',
+    travelTime: 83
+  },
+  3: {
+    address: 'Stichting Survive and Feelgood, Weegbree 14, 5071 HP Udenhout',
+    travelTime: 73
+  },
+  4: {
+    address: 'Rutbeek Survivalvereniging, Blikkersmaatweg 15, 7546 RJ Enschede',
+    travelTime: 144
+  },
+  5: {
+    address: 'Tuxis Ede\'s Best Run, Langekampweg 6, 6715 AV Ede',
+    travelTime: 78
+  },
+  6: {
+    address: 'Bikkeltjesrun, Groningerstraatweg 401, 8926 XC Leeuwarden',
+    travelTime: 147
+  },
+  7: {
+    address: 'Stichting Survivalteam Boerakker, Bakkerom 6, 9362 VA Boerakker',
+    travelTime: 145
+  },
+  8: {
+    address: 'Outdoor Valley, Hoeksekade 141, 2661 JL Bergschenhoek',
+    travelTime: 30
+  },
+  // 9: {
+  //   address: 'Stichting Survival Neede, G.L. Rutgersweg 11, 7161 PC Neede',
+  //   travelTime: 90
+  // },
+  // 10: {
+  //   address: 'Stichting SurvivalRun Deest, Van Heemstraweg 3, 6653 KA Deest',
+  //   travelTime: 90
+  // },
+  // 11: {
+  //   address: 'Outdoor Challenge Park, Bornsedijk 105, 7576 PG Oldenzaal',
+  //   travelTime: 90
+  // },
+  // 12: {
+  //   address: 'Hommes Survival & Outdoor Survivalbaan Zernike, De Bunders 2, 9747 AC Groningen',
+  //   travelTime: 90
+  // },
+  // 13: {
+  //   address: 'Trainingsgroep Survival Vollenhove',
+  //   travelTime: 90
+  // },
+  // 14: {
+  //   address: 'Survivalrun Meetjesland, Moerwege 57B, 9981 Maldegem, Belgium',
+  //   travelTime: 90
+  // },
+  // 15: {
+  //   address: 'Hang-On Run, Hydeparklaan 22-28, 3941 ZJ Doorn',
+  //   travelTime: 90
+  // }
+}
